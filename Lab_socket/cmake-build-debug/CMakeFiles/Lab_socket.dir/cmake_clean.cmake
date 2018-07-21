8a8a1d1006041f5e6bbf18c726c358ea75685c20
XuandongZhao <3150104910@zju.edu.cn>
2018-07-21 11:14:12 -0400

    create

---
+++ Lab_socket/cmake-build-debug/CMakeFiles/Lab_socket.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab_socket.dir/client.c.o"
+  "Lab_socket.pdb"
+  "Lab_socket"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Lab_socket.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Lab_web/cmake-build-debug/CMakeFiles/Lab_web.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab_web.dir/main.cpp.o"
+  "Lab_web.pdb"
+  "Lab_web"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lab_web.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
